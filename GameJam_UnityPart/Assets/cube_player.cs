ed61faa9bcf91d4bf17f5fff6115b4e169409505
Gerstein Liam <liam.gerstein@gmail.com>
2021-10-14 23:14:16 +0200

    create a simple animation with a cube

---
+++ GameJam_UnityPart/Assets/cube_player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class cube_player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Animator _Animator;
+    private bool JumpCondition = false;
+    private bool RotateCondition = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _Animator.SetBool("rotate_condition", RotateCondition);
+        _Animator.SetBool("jump_condition", JumpCondition);
+
+        JumpCondition = false;
+        RotateCondition = false;
+    }
+
+
+    public void JumpEvent(InputAction.CallbackContext _InputAction) {
+
+        Debug.Log("JumpEvent");
+        JumpCondition = true;
+
+    }
+
+    public void RotateEvent(InputAction.CallbackContext _InputAction)
+    {
+
+        Debug.Log("RotateEvent");
+        RotateCondition = true;
+
+    }
+}
