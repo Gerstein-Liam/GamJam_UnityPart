81170b969133e2d07c3c89011adfd8dd1f83d358
Gerstein Liam <liam.gerstein@gmail.com>
2021-10-17 10:27:58 +0200

    ajout d'information de clip

--- GameJam_UnityPart/Assets/Scripts/animation_blenderfighter00.cs
+++ GameJam_UnityPart/Assets/Scripts/animation_blenderfighter00.cs
@@ -7,6 +7,10 @@ public class animation_blenderfighter00 : MonoBehaviour
     public Animator _Animator;
     private bool PunchCondition = false;
     private bool KickCondition = false;
+    string m_ClipName;
+    AnimatorClipInfo[] m_CurrentClipInfo;
+
+    float m_CurrentClipLength;
     void Start()
     {
         
@@ -15,9 +19,16 @@ public class animation_blenderfighter00 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        m_CurrentClipInfo = this._Animator.GetCurrentAnimatorClipInfo(0);
+        //Access the current length of the clip
+        m_CurrentClipLength = m_CurrentClipInfo[0].clip.length;
+        //Access the Animation clip name
+        m_ClipName = m_CurrentClipInfo[0].clip.name;
+
         _Animator.SetBool("punch_condition", PunchCondition);
         _Animator.SetBool("kick_condition", KickCondition);
-
+       
         PunchCondition = false;
         KickCondition = false;
     }
@@ -37,5 +48,11 @@ public class animation_blenderfighter00 : MonoBehaviour
         Debug.Log("RotateEvent");
         KickCondition = true;
 
+    }
+    void OnGUI()
+    {
+        //Output the current Animation name and length to the screen
+        GUI.Label(new Rect(0, 0, 200, 20), "Clip Name : " + m_ClipName);
+        GUI.Label(new Rect(0, 30, 200, 20), "Clip Length : " + m_CurrentClipLength);
     }
 }
